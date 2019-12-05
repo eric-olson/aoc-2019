4ba35047b63b05f6c16e1c7d1284f1d238acc6d5
Eric Olson <eric.olson.411@gmail.com>
2019-12-04 20:31:27 -0700

    day 4

---
+++ 4/part2.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+
+import sys
+
+# get bounds in format '123456-234567'
+bounds = [int(i) for i in sys.argv[1].split('-')]
+print(bounds)
+
+def valid(password):
+    # take advantage of python string iterability
+    password = str(password)
+    # sorted == list implies increasing order, len > len(set) implies at least one duplicate
+    return sorted(password) == list(password) and 2 in [password.count(i) for i in password]
+
+count = sum(valid(p) for p in range(*bounds))
+
+print(count)
+
