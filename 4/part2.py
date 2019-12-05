a2fec6f7a6b770cf860c30a6cff311d2e3351417
Eric Olson <eric.olson.411@gmail.com>
2019-12-04 20:33:03 -0700

    fixed comment in day4.2

--- 4/part2.py
+++ 4/part2.py
@@ -9,7 +9,7 @@ print(bounds)
 def valid(password):
     # take advantage of python string iterability
     password = str(password)
-    # sorted == list implies increasing order, len > len(set) implies at least one duplicate
+    # sorted == list implies increasing order, 2 in count implies one value present exactly 2x
     return sorted(password) == list(password) and 2 in [password.count(i) for i in password]
 
 count = sum(valid(p) for p in range(*bounds))
